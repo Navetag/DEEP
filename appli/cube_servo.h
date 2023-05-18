a062c241a8d178f28e35b46fa4a61e124c92a56b
Evan Augereau <evan.augereau1@orange.fr>
2023-05-18 16:07:57 +0200

    Ajout des headers des modules cube_servo et screen

---
+++ appli/cube_servo.h
@@ -0,0 +1,90 @@
+#include "config.h"
+
+typedef enum{
+    BOTTOM_90_A,
+    BOTTOM_180,
+    BOTTOM_90,
+
+    TOP_90_A,
+    TOP_180,
+    TOP_90,
+
+    FRONT_90_A,
+    FRONT_180,
+    FRONT_90,
+
+    BACK_90_A,
+    BACK_180,
+    BACK_90,
+
+    LEFT_90_A,
+    LEFT_180,
+    LEFT_90,
+
+    RIGHT_90_A,
+    RIGHT_180,
+    RIGHT_90
+
+    GAUCHE_90,
+    DROITE_90,
+    FLIP,
+
+    READING_POSITION,
+    DEFAULT
+
+}cube_servo_complex_mvt_e;
+
+
+typedef enum{
+    CLOCKWISE_90,
+    REVERSE_90,
+    
+    HOLD_CUBE,
+    FLIP_UP,
+    FLIP_DOWN,
+    SCAN_POS,
+    DEFAULT         //Le doigt ne touche pas et le cube peut tourner
+
+}cube_servo_primary_mvt_e
+
+
+
+typedef enum{
+    WAIT,
+    MAKE,
+    FINISHED
+}cube_servo_state_e;
+
+typedef enum{
+    POS_CENTER,
+    POS_LEFT,
+    POS_RIGHT
+}cube_servo_rot_pos_e;
+
+typedef enum{
+    POS_FLIP_UP,
+
+    POS_DEFAULT,
+    POS_CUBE_LOCK
+}cube_servo_cage_pos_e;
+
+
+
+/*
+    Machine à état
+        - gère une pile FIFO contenant les mvts en attente (tableau de )
+        - récupère l'état du cube
+*/
+void cube_servo_process(cueb_s cube);
+
+/*
+    Ajoute un mouvement dans la pile
+*/
+bool_e cube_servo_addMvt(cube_servo_complex_mvt_e mvt);
+
+/*
+    Renvoie l'état de la machine servo
+*/
+cube_servo_state_e cube_servo_getState(void);
+
+void cube_servo_init();
---
+++ appli/screen.h
@@ -0,0 +1,49 @@
+#include "config.h"
+
+/*
+    Enumération de l'écran demandé
+*/
+typedef enum{
+    MENU,
+    SOLVING_MENU,
+    SOLVING,
+    SCANNING,
+    SHUFFLING,
+    SHUFFLING_MENU
+}screen_type_e;
+
+/*
+    Enumération de l'action récupérée par le tactile
+*/
+typedef enum{
+    CLOSE,
+    START,
+    SHUFFLE,
+    SOLVE
+}user_action_e;
+
+typedef enum{
+    WAIT,
+    GET_PRESSED_BUTTON
+}screen_state_e;
+
+
+/*
+    Récupération de l'action effectuée via le tactile de l'écran
+*/
+user_action_e screen_getLastUserAction(void);
+
+/*
+    Demande d'affichage du menu demandé
+*/
+void screen_display(screen_type_e, cube_s * cube);
+
+/*
+    Machine à état
+*/
+void screen_process(void);
+
+
+void screen_init(void);
+
+
